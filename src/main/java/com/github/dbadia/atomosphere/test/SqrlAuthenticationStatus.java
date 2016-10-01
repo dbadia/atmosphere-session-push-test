af8c3bcad8005f59f93c5c9be1232564d35cf791
Dave Badia <dbadiaoss@gmail.com>
2016-10-01 10:28:43 -0400

    logging

--- src/main/java/com/github/dbadia/atomosphere/test/SqrlAuthenticationStatus.java
+++ src/main/java/com/github/dbadia/atomosphere/test/SqrlAuthenticationStatus.java
@@ -1,7 +1,7 @@
 package com.github.dbadia.atomosphere.test;
 
 public enum SqrlAuthenticationStatus {
-	CORRELATOR_ISSUED, COMMUNICATING, AUTH_COMPLETE, ERROR_BAD_REQUEST, ERROR_SQRL_INTERNAL;
+	CORRELATOR_ISSUED, COMMUNICATING, ERROR_BAD_REQUEST, ERROR_SQRL_INTERNAL, AUTH_COMPLETE;
 
 	public boolean isErrorStatus() {
 		return this.toString().startsWith("ERROR_");
